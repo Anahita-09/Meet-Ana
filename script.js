84a52e0799146f2d642b19ff6a558b13b22a828e
Anahita Mayekar <anahitamayekar03@gmail.com>
2025-10-28 17:57:42 -0400

    Made the functionality for the bag work finally, though for now without any images. I think I'll add/modify all the images near the ending of the project.

--- script.js
+++ script.js
@@ -38,14 +38,24 @@ function showBagItems() {
     })
 }
 
-const bagItem = {
-    laptop: "♾️",
-    wallet: "♾️ My wallet contains the flower my boyfriend gave me.",
-    keychains: "♾️ I'm very emotionally connected to these keychains given by my boyfriend. Backstory - I lost one of them once, and he brought the exactly same one again <3",
-    headphones: "♾️ Sony Headphones! I usually listen to Punjabi songs (Though I barely understand them)",
-    woodenBox: "♾️ My boyfriend made this box for me which has our ship name written on it.",
-    phone: "♾️ My phone cover represents my hope to soon fly back to India.", 
-    cake: "♾️ I always have this cake from Tours Les Jours, my favorite bakery!"
-};
-
-
+laptop.addEventListener("click", function showLaptopInfo() {
+    itemInfo.innerHTML = "<h1>Laptop</h1><p></p>";
+});
+wallet.addEventListener("click", function showWalletInfo() {
+    itemInfo.innerHTML = "<h1>Wallet</h1><p>♾️ My wallet contains the flower my boyfriend gave me.</p>";
+});
+keychains.addEventListener("click", function showKeychainsInto() {
+    itemInfo.innerHTML = "<h1>Keychains</h1><p> ♾️ I'm very emotionally connected to these keychains given by my boyfriend. Backstory - I lost one of them once, and he brought the exact same one for me again <3</p>";
+});
+headphones.addEventListener("click", function showHeadphonesInfo() {
+    itemInfo.innerHTML = "<h1>Headphones</h1><p>♾️ Sony Headphones! I usually listen to Punjabi songs on this (Though I barely understand them).</p>";
+});
+woodenBox.addEventListener("click", function showWoodenBoxInfo () {
+    itemInfo.innerHTML = "<h1>Wooden Box</h1><p>♾️ My Boyfriend made this box for me, it has our ship name written on it.</p>";
+});
+phone.addEventListener("click", function showPhoneInfo () {
+    itemInfo.innerHTML = "<h1>Phone</h1><p>♾️ My phone case represents my hope to fly back to India soon.</p>";
+});
+cake.addEventListener("click", function showCakeInfo () {
+    itemInfo.innerHTML = "<h1>Cake</h1><p>♾️ I always have this cake from Tours Les Jours, my favorite bakery!</p>";
+});
